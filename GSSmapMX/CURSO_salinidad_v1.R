2fb13fb673ebc71218b05664ef5125e1064dd3c2
marioguevara <mguevara@udel.edu>
2020-08-05 09:35:12 -0400

    Update CURSO_salinidad_v1.R

--- GSSmapMX/CURSO_salinidad_v1.R
+++ GSSmapMX/CURSO_salinidad_v1.R
@@ -1,2 +1,13 @@
-###Codes for digital soil salinity mapping across Latinamerica
-###M. Guevara
+###Entrenamiento en salinidad de suelos
+###Practica 1. Mapeo digital de suelos salinos. 
+###M. Guevara and E. Tailensik
+###
+###Objetivos: 1) proveer un panorama general del estado actual de datos e información de suelos salinos en Latinoamérica y
+###           2) proveer una demostración de mapeo digital de suelos salinos.
+###
+###Al final de esta clase los participantes contarán con elementos teóricos y prácticos para la generación e 
+###interpretación de mapas digitales de indicadores de salinidad de suelos en sus áreas de interes.
+#####################################################################################################
+
+#load required libraries
+library(raster) ...
