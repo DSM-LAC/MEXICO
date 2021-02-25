1d96a11bd012d50183ae14bb8e8d0b9aabde7bba
marioguevara <mguevara@udel.edu>
2021-02-25 16:30:16 -0500

    Update mapa_base_de_C_profundidad_v0.R

--- soc_map_mx_90m_inegi_s2_bdSaini/mapa_base_de_C_profundidad_v0.R
+++ soc_map_mx_90m_inegi_s2_bdSaini/mapa_base_de_C_profundidad_v0.R
@@ -2,3 +2,16 @@
 #nacional de profundidad efectiva de raices en una resolucion espacial de 1x1km. 
 ##Autores: Josselin Arias, Armando Navarrete, Mario Guevara et al., 2021
 
+
+
+
+#carga librerias
+library(raster)
+library(rgdal)
+
+dat <- read.csv('/Users/marioguevara/Downloads/sal/v0_carbono_anavarrete/soc_12_2020.xls - soc.csv')
+#read prediction factors
+covar <- readRDS('PREDICTORES_ACTUALIZADOS.rds')
+plot(covar)
+names(covar)
+
