99fb15762ff030eb5c93916108263989fcb66c3b
marioguevara <mguevara@udel.edu>
2021-02-25 16:28:53 -0500

    Create mapa_base_de_C_profundidad_v0.R

---
+++ soc_map_mx_90m_inegi_s2_bdSaini/mapa_base_de_C_profundidad_v0.R
@@ -0,0 +1,4 @@
+###Codigo de R de Josselin y Mario para generar un mapa nacional de carbono organico en el suelo, y un mapa
+#nacional de profundidad efectiva de raices en una resolucion espacial de 1x1km. 
+##Autores: Josselin Arias, Armando Navarrete, Mario Guevara et al., 2021
+
