68dbdac7ea3a136c2c34f1052e593fba4c997e2a
marioguevara <mguevara@udel.edu>
2021-02-25 08:37:38 -0500

    Update clase_geostat_2021.R

--- clase_geostat_2021.R
+++ clase_geostat_2021.R
@@ -1,43 +1,68 @@
-#pregunta a R donde estamos
+#' ---
+#' title: "Clase geostat 2021 PCT"
+#' author: "Ovalle, Ramos, Guevara, 2021"
+#' output:
+#'   pdf_document:
+#'     keep_tex: true
+#' ---
+
+
+
+#'Este codigo sirve para la clase de geostat 2021
+#'y el tema es variogramas.
+#'El objetivo de este codigo es dejar evidencia reproducible de resultados de analisis de variogramas
+#'con datos de respiracion de suelos a diversas escalas de trabajo, con diferentes configuraciones de 
+#'datos disponibles y a lo largo de diversos escenarios naturales
+#'Feb, 2021
+#+ <p>&nbsp;</p>
+#+ <p>&nbsp;</p>
+#'pregunta a R donde estamos
+#+ , attr.source='.numberLines'
 getwd()
-#definir directorio de trabajo
+#'definir directorio de trabajo
 setwd("/Users/marioguevara/Downloads/clase")
-#librarias necesarias
-install.packages(c('raster', 'automap', 'rgdal'))
+#'librarias necesarias
+
+#'install.packages(c('raster', 'automap', 'rgdal'))
 library(raster)
 library(rgdal)
 library(automap)
-#pregunta a R como funciona getData
-#?getData
-#descarga limite de pais
+#'pregunta a R como funciona getData
+#'?getData
+#'descarga limite de pais
+
 lim <- getData('GADM', country='MEX', level=2)
-#que tipo de objeto es este?
+#'que tipo de objeto es este?
+
 class(lim)
-#leer la base de datos de trabajo
+#'leer la base de datos de trabajo
+
 srdb <- read.csv('https://raw.githubusercontent.com/bpbond/srdb/master/srdb-data.csv')
-#nombres de las variables en base de datos 
+
+#'nombres de las variables en base de datos 
+
  names(srdb)
- #selecciona variables especificas de otra base de datos
-soilCN <-  srdb[c('Longitude' , 'Latitude', 'Soil_CN')]
-#quitamos valores no asignados
+ #'selecciona variables especificas de otra base de datos
+ 
+ soilCN <-  srdb[c('Longitude' , 'Latitude', 'Soil_CN')]
+#'quitamos valores no asignados
 soilCN <- na.omit(soilCN)
- #define un objeto espacial
+ #'define un objeto espacial
  coordinates(soilCN) <- ~ Longitude + Latitude
+  library(automap)
+ #+ results=FALSE, warnings=FALSE
 variogram = autofitVariogram(Soil_CN ~1, soilCN)
 
-#### PREPARACION DE DATOS PARA VARIOGRAMA
+plot(variogram)
+#'PREPARACION DE DATOS PARA VARIOGRAMA
 
-DEFINIR PROYECCION GEOGRAFICA
-reproyectar si es necesario 
-UNIDADES METRICAS 
+#'DEFINIR PROYECCION GEOGRAFICA
+#'reproyectar si es necesario 
+#UNIDADES METRICAS 
 
-#?autofitVariogram
+#'?autofitVariogram
 
-library(automap)
-
-
-
-
+#'library(automap)
 
 
 
